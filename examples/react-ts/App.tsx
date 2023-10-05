cba465a7937e9fccc47f90fd463019e4cbcca64c
colinscz <19342760+colinscz@users.noreply.github.com>
2023-10-05 09:02:54 +0200

    chore: correct docs and broken links

--- examples/react-ts/App.tsx
+++ examples/react-ts/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SixRoot, SixRootCollapsedPayload } from '@six-group/ui-library-react';
+import React from 'react';
+
+// components
+import Home from './components/Home';
+
+defineCustomElements();
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const leftSidebar = useModalState(true);
+  const rightSidebar = useModalState(false);
+  const [, setSearch] = useState('');
+
+  const handleCollapsed = ({ detail }: CustomEvent<SixRootCollapsedPayload>) => {
+    if (detail.collapsed === leftSidebar.isOpen) {
+      leftSidebar.setIsOpen(!detail.collapsed);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <six-root onSix-root-collapsed={handleCollapsed}>
+      <Header toggleMenu={leftSidebar.toggle} toggleSearch={setSearch} toggleNotifications={rightSidebar.toggle} />
+      <LeftSidebar visible={leftSidebar.isOpen} />
+      <RightSidebar visible={rightSidebar.isOpen} />
+      <section slot="main">
+        <Suspense fallback={<six-spinner />}>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home toggleLeftSidebar={leftSidebar.toggle} toggleRightSidebar={rightSidebar.toggle} />}
+            />
+            <Route path="/users" element={<Users />} />
+          </Routes>
+        </Suspense>
+      </section>
+    </six-root>
+  );
 }
 
 export default App
